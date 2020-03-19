3eeafd7cd645d956bf46181c4f705c5ee60cb792
Asiye Eldes <eldesasiye@gmail.com>
2020-03-19 17:02:56 +0200

    Bu sefer olsun yani

---
+++ src/gitDay4Package/GitDayFour.java
@@ -0,0 +1,9 @@
+package gitDay4Package;
+
+public class GitDayFour {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Atina");
+	}
+
+}
---
+++ src/gitDay4Package/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author user
+ *
+ */
+package gitDay4Package;

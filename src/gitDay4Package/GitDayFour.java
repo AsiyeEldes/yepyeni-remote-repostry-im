d074fea472ab9c798833ee1e5e8ace923a46f5e2
AsiyeEldes <62253580+AsiyeEldes@users.noreply.github.com>
2020-03-19 17:34:01 +0200

    aklimdan suphe etmeye basladim

--- src/gitDay4Package/GitDayFour.java
+++ src/gitDay4Package/GitDayFour.java
@@ -5,6 +5,7 @@ public class GitDayFour {
 	public static void main(String[] args) {
 		System.out.println("Hello Atina");
 		System.out.println("Nezaman Atina terhis");
+		System.out.println("Kafam corba oldu yaaa);
 	}
 
 }
